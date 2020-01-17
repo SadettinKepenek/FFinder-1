3e89592da002753928a8f97e0dd01cd2cb4e6d35
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2020-01-17 23:59:21 +0300

    Add Comment Rate Validator

---
+++ FFinder.BLL/Validators/CommentRate/AddCommentRateValidator.cs
@@ -0,0 +1,20 @@
+﻿using FFinder.Core.DataTransferObjects.CommentRate;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFinder.BLL.Validators.CommentRate
+{
+    public class AddCommentRateValidator:AbstractValidator<CommentRateAddDto>
+    {
+        public AddCommentRateValidator()
+        {
+            RuleFor(x => x.CommentId).NotEmpty().WithMessage("CommentId belirtilmelidir.");
+            RuleFor(x => x.IsLike).NotEmpty().WithMessage("Yoruma like atılıp atılmadığı belirtilmelidir.");
+            RuleFor(x => x.RateDate).NotEmpty().WithMessage("Rate'e ait date belirtilmelidir.");
+            RuleFor(x => x.IsActive).NotEmpty().WithMessage("Yorumun aktiflik durumu belirtilmelidir.");
+            RuleFor(x => x.OwnerId).NotEmpty().WithMessage("OwnerId belirtilmelidir.");
+        }
+    }
+}
