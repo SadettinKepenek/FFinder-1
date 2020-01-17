06d56a638d61657d2a9d982da3ae4f6b46990499
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-18 01:00:12 +0300

    Add PostMapping

---
+++ FFinder/MappingProfiles/PostMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FFinder.Core.DataTransferObjects.Post;
+using FFinder.Entity.Concrete;
+
+namespace FFinder.MappingProfiles
+{
+    public class PostMappingProfile:Profile
+    {
+        public PostMappingProfile()
+        {
+            CreateMap<PostAddDto, Post>();
+            CreateMap<PostUpdateDto, Post>();
+            CreateMap<PostListDto, Post>().ReverseMap();
+            CreateMap<PostDetailDto, Post>().ReverseMap();
+        }
+    }
+}
