37dd2ee9f1030cb8514d7f8d021f82c5d7753bcc
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-18 00:35:21 +0300

    Add Post Rate Service

---
+++ FFinder.BLL/Abstract/IPostRateService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using FFinder.Core.DataTransferObjects.PostRate;
+
+namespace FFinder.BLL.Abstract
+{
+    public interface IPostRateService
+    {
+        List<PostRateListDto> Get();
+        PostRateDetailDto Get(string id);
+        void Add(PostRateAddDto dto);
+        void Update(PostRateUpdateDto dto);
+        void Delete(string id);
+    }
+}
