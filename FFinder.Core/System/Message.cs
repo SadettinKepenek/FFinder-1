b0afdd1e5c18d4dae238d42b655f91d86beae396
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-18 02:19:13 +0300

    Add Login Method Add Register Method

---
+++ FFinder.Core/System/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFinder.Core.System
+{
+    public class Message
+    {
+        public static string SuccessMessage { get; } = "İşlem başarılı..";
+        public static string BadRequestMessage { get; } = "İşlem yapılırken hata oluştu..";
+    }
+}
--- FFinder/Controllers/UsersController.cs
+++ FFinder/Controllers/UsersController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using FFinder.BLL.Abstract;
 using FFinder.Core.DataTransferObjects.User;
+using FFinder.Core.System;
+using FFinder.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,7 +24,54 @@ namespace FFinder.Controllers
 
         public async Task<IActionResult> Login(UserLoginRequestDto model)
         {
+            HttpResponseModel responseModel;
+            try
+            {
+                var loginResult = _authService.Login(model);
 
+                responseModel = new HttpResponseModelData<UserLoginResponseDto>()
+                {
+                    Message =Message.SuccessMessage,
+                    StatusCode = 200,
+                    Data = loginResult
+                };
+
+            }
+            catch (Exception e)
+            {
+                 responseModel = new HttpResponseModelSimple
+                {
+                    Message = e.Message,
+                    StatusCode = 400
+                };
+                return BadRequest(responseModel);
+            }
+            return Ok();
+        }
+
+        public async Task<IActionResult> Register(UserAddDto userAddDto)
+        {
+            HttpResponseModel responseModel;
+            try
+            {
+                await _authService.Register(userAddDto,userAddDto.Password);
+
+                responseModel = new HttpResponseModelSimple()
+                {
+                    Message = Message.SuccessMessage,
+                    StatusCode = 200,
+                };
+
+            }
+            catch (Exception e)
+            {
+                responseModel = new HttpResponseModelSimple
+                {
+                    Message = e.Message,
+                    StatusCode = 400
+                };
+                return BadRequest(responseModel);
+            }
             return Ok();
         }
     }
