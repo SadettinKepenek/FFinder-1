3c6557d7f20fdf360757bdce30c176bd9ff332ab
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2020-01-18 00:56:24 +0300

    Add Comment Rate Service

--- FFinder.Core/DataTransferObjects/CommentRate/CommentRateListDto.cs
+++ FFinder.Core/DataTransferObjects/CommentRate/CommentRateListDto.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using FFinder.Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using FFinder.Core.DataTransferObjects.Comment;
+using FFinder.Core.DataTransferObjects.User;
 
 namespace FFinder.Core.DataTransferObjects.CommentRate
 {
-    public class CommandRateUpdateDto
+    public class CommentRateListDto
     {
         public string CommentRateId { get; set; }
         public bool IsLike { get; set; }
@@ -12,5 +15,7 @@ namespace FFinder.Core.DataTransferObjects.CommentRate
         public string CommentId { get; set; }
         public string OwnerId { get; set; }
         public bool IsActive { get; set; }
+        public virtual UserListDto Owner { get; set; }
+        public virtual CommentListDto Comment { get; set; }
     }
 }
