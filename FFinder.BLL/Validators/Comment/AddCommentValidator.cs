5d6ddb2d3c478fb910a625aac8d47f1417e42bc7
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2020-01-17 23:50:04 +0300

    Add Comment Validation

---
+++ FFinder.BLL/Validators/Comment/AddCommentValidator.cs
@@ -0,0 +1,24 @@
+﻿using FFinder.Core.DataTransferObjects.Comment;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFinder.BLL.Validators.Comment
+{
+    public class AddCommentValidator: AbstractValidator<CommentAddDto>
+    {
+        public AddCommentValidator()
+        {
+            RuleFor(x => x.CommentBody).NotEmpty().WithMessage("Yorum içeriği belirtilmelidir.");
+            RuleFor(x => x.CommentBody).MaximumLength(250)
+                .WithMessage("Yorum içeriği maksimum 250 karakterden oluşmalıdır");
+            RuleFor(x => x.PostId).NotEmpty().WithMessage("PostId belirtilmelidir.");
+            RuleFor(x => x.CommentDate).NotEmpty().WithMessage("CommentDate belirtilmelidir.");
+            RuleFor(x => x.IsActive).NotEmpty().WithMessage("Yorumun aktiflik durumu belirtilmelidir.");         
+            RuleFor(x => x.OwnerId).NotEmpty().WithMessage("OwnerId belirtilmelidir.");
+
+
+        }
+    }
+}
