8efb2530c3543eb013705ca02387a3ff2f7aa071
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-17 21:37:57 +0300

    Add EfPostRateRepository

---
+++ FFinder.DAL/Concrete/EntityFramework/EfPostRateRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using FFinder.Core.DAL.Concrete.EntityFramework;
+using FFinder.DAL.Abstract;
+using FFinder.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace FFinder.DAL.Concrete.EntityFramework
+{
+    public class EfPostRateRepository : EfEntityRepositoryBase<PostRate, SqlDbContext>, IPostRateRepository
+    {
+        public PostRate Get(Expression<Func<PostRate, bool>> filter = null)
+        {
+            using SqlDbContext context = new SqlDbContext();
+            return filter == null ? context.PostRates
+                .Include(x => x.Owner)
+                .Include(y => y.Post)
+                .ThenInclude(c => c.Owner)
+                .FirstOrDefault() : context.PostRates
+                .Include(x => x.Owner)
+                .Include(y => y.Post)
+                .ThenInclude(c => c.Owner).FirstOrDefault(filter);
+        }
+
+        public List<PostRate> GetList(Expression<Func<PostRate, bool>> filter = null)
+        {
+            using SqlDbContext context = new SqlDbContext();
+            return filter == null ? context.PostRates
+                .Include(x => x.Owner)
+                .Include(y => y.Post)
+                .ThenInclude(c => c.Owner)
+                .ToList() : context.PostRates
+                .Include(x => x.Owner)
+                .Include(y => y.Post)
+                .ThenInclude(c => c.Owner).Where(filter).ToList();
+        }
+    }
+}
