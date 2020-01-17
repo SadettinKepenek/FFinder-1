86262df26d52c831a7ef4bedae13632bbd7d23d2
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-17 21:52:23 +0300

    Add PostRate Dtos

---
+++ FFinder.Core/DataTransferObjects/PostRate/PostRateAddDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FFinder.Entity.Concrete;
+
+namespace FFinder.Core.DataTransferObjects.PostRate
+{
+    public class PostRateAddDto
+    {
+        public bool IsLike { get; set; }
+        public DateTime RateDate { get; set; }
+        public string PostId { get; set; }
+        public string OwnerId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ FFinder.Core/DataTransferObjects/PostRate/PostRateDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FFinder.Entity.Concrete;
+
+namespace FFinder.Core.DataTransferObjects.PostRate
+{
+    public class PostRateDetailDto
+    {
+        public string PostRateId { get; set; }
+        public bool IsLike { get; set; }
+        public DateTime RateDate { get; set; }
+        public string PostId { get; set; }
+        public string OwnerId { get; set; }
+        public bool IsActive { get; set; }
+        public virtual AuthIdentityUser Owner { get; set; }
+        public virtual Entity.Concrete.Post Post { get; set; }
+    }
+}
