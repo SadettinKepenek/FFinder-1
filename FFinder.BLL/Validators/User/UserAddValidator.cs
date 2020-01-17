c32ebbbdca22b14cb36265d4e21b6b2b3dd7b1fa
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-17 23:51:31 +0300

    Add UserValidators

---
+++ FFinder.BLL/Validators/User/UserAddValidator.cs
@@ -0,0 +1,22 @@
+﻿using FFinder.Core.DataTransferObjects.User;
+using FluentValidation;
+
+namespace FFinder.BLL.Validators.User
+{
+    public class UserAddValidator:AbstractValidator<UserAddDto>
+    {
+        public UserAddValidator()
+        {
+            RuleFor(x => x.IsActive).NotNull().NotEmpty().WithMessage("Aktiflik durumu boş geçilemez");
+            RuleFor(x => x.AboutMe).NotNull().NotEmpty().WithMessage("Hakkımda kısmı boş geçilemez");
+            RuleFor(x => x.City).NotNull().NotEmpty().WithMessage("Şehir kısmı boş geçilemez");
+            RuleFor(x => x.Country).NotNull().NotEmpty().WithMessage("Ülke kısmı boş geçilemez");
+            RuleFor(x => x.Email).NotNull().NotEmpty().WithMessage("Email kısmı boş geçilemez");
+            RuleFor(x => x.Firstname).NotNull().NotEmpty().WithMessage("Ad kısmı boş geçilemez");
+            RuleFor(x => x.Lastname).NotNull().NotEmpty().WithMessage("Soyad kısmı boş geçilemez");
+            RuleFor(x => x.Gender).NotNull().NotEmpty().WithMessage("Cinsiyet kısmı boş geçilemez");
+            RuleFor(x => x.Password).NotNull().NotEmpty().WithMessage("Şifre kısmı boş geçilemez");
+            RuleFor(x => x.UserName).NotNull().NotEmpty().WithMessage("Username kısmı boş geçilemez");
+        }
+    }
+}
--- FFinder.Core/DataTransferObjects/User/UserUpdateDto.cs
+++ FFinder.Core/DataTransferObjects/User/UserUpdateDto.cs
@@ -10,6 +10,7 @@ namespace FFinder.Core.DataTransferObjects.User
 
         public string UserName { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public string PhoneNumber { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
