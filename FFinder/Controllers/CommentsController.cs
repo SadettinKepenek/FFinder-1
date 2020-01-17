ff083baef3d734e4cbbbe6d751d367d1aadc8b82
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2020-01-18 02:32:55 +0300

    Add Comment Controller

---
+++ FFinder/Controllers/CommentsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FFinder.BLL.Abstract;
+using FFinder.Core.DataTransferObjects.Comment;
+using FFinder.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FFinder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        ICommentService _commentService;
+
+        public CommentsController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        [HttpGet("")]
+        public async Task<IActionResult> GetAll()
+        {
+            var entities = _commentService.GetAll();
+            if (entities == null)
+            {
+                return NotFound(new HttpResponseModelSimple
+                {
+                    StatusCode = 404,
+                    Message = "Herhangi bir kayıt bulunamadı"
+                });
+            }
+            HttpResponseModelData<List<CommentListDto>> responseModelWithData = new HttpResponseModelData<List<CommentListDto>>();
+            responseModelWithData.StatusCode = 200;
+            responseModelWithData.Message = "Kayıtlar başarıyla getirildi";
+            responseModelWithData.Data = entities;
+            return Ok(responseModelWithData);
+        }
+        [HttpGet("GetById")]
+        public async Task<IActionResult> GetById([FromBody] string id)
+        {
+            var entities = _commentService.GetById(id);
+            if (entities == null)
+            {
+                return NotFound(new HttpResponseModelSimple
+                {
+                    StatusCode = 404,
+                    Message = "Herhangi bir kayıt bulunamadı"
+                });
+            }
+            HttpResponseModelData<CommentDetailDto> responseModelWithData = new HttpResponseModelData<CommentDetailDto>();
+            responseModelWithData.StatusCode = 200;
+            responseModelWithData.Message = "Kayıt başarıyla getirildi";
+            responseModelWithData.Data = entities;
+            return Ok(responseModelWithData);
+        }
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add([FromBody] CommentAddDto model)
+        {
+            try
+            {
+                _commentService.Add(model);
+                return Ok(new HttpResponseModelSimple
+                {
+                    StatusCode = 200,
+                    Message = "Kayıt başarıyla eklendi"
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new HttpResponseModelSimple
+                {
+                    StatusCode = 400,
+                    Message = "Kayıt eklenirken bir hata oluştu"
+                });
+            }
+        }
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update([FromBody] CommentUpdateDto model)
+        {
+            try
+            {
+                _commentService.Update(model);
+                return Ok(new HttpResponseModelSimple
+                {
+                    StatusCode = 200,
+                    Message = "Kayıt başarıyla güncellendi"
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new HttpResponseModelSimple
+                {
+                    StatusCode = 400,
+                    Message = "Kayıt güncellenirken bir hata oluştu"
+                });
+            }
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] string id)
+        {
+            try
+            {
+                _commentService.Delete(id);
+                return Ok(new HttpResponseModelSimple
+                {
+                    StatusCode = 200,
+                    Message = "Kayıt başarıyla silindi"
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new HttpResponseModelSimple
+                {
+                    StatusCode = 400,
+                    Message = "Kayıt silinirken bir hata oluştu"
+                });
+            }
+        }
+    }
+}
