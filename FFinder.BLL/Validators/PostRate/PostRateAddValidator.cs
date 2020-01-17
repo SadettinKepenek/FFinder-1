46026c2cee29e8173f63e1e14ddcbd2a764d1aa6
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-17 23:48:13 +0300

    Add Post Rate Validators

---
+++ FFinder.BLL/Validators/PostRate/PostRateAddValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FFinder.Core.DataTransferObjects.PostRate;
+using FluentValidation;
+
+namespace FFinder.BLL.Validators.PostRate
+{
+    public class PostRateAddValidator:AbstractValidator<PostRateAddDto>
+    {
+        public PostRateAddValidator()
+        {
+            RuleFor(x => x.IsActive).NotNull().NotEmpty().WithMessage("Aktiflik durumu boş geçilemez");
+            RuleFor(x => x.OwnerId).NotNull().NotEmpty().WithMessage("Kullanıcı Id'si boş geçilemez");
+            RuleFor(x => x.PostId).NotNull().NotEmpty().WithMessage("Post Id boş geçilemez");
+            RuleFor(x => x.IsLike).NotNull().NotEmpty().WithMessage("Like yada Dislike seçilmelidir");
+            RuleFor(x => x.RateDate).NotNull().NotEmpty().GreaterThan(DateTime.Now.AddMinutes(-5))
+                .WithMessage("Beğeni tarihi bugün veya daha ilerisi olabilir");
+        }
+    }
+}
