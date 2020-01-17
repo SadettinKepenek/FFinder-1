334a5718d579979ff7a62257e25112ceb6c4fb40
berkayalcin <yalcinberkaycom@gmail.com>
2020-01-17 23:54:49 +0300

    Add Follower Validators

---
+++ FFinder.BLL/Validators/Follower/FollowerUpdateValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FFinder.Core.DataTransferObjects.Follower;
+using FluentValidation;
+
+namespace FFinder.BLL.Validators.Follower
+{
+    public class FollowerUpdateValidator:AbstractValidator<FollowerUpdateDto>
+    {
+        public FollowerUpdateValidator()
+        {
+            RuleFor(x => x.FollowerId).NotNull().NotEmpty().WithMessage("Takip Idsi boş geçilemez");
+            RuleFor(x => x.User1Id).NotNull().NotEmpty().WithMessage("Takip isteğini gönderen boş geçilemez");
+            RuleFor(x => x.User2Id).NotNull().NotEmpty().WithMessage("Takip isteği gönderilen boş geçilemez");
+            RuleFor(x => x.FriendshipDate).NotNull().NotEmpty().GreaterThan(DateTime.Now.AddMinutes(-5)).WithMessage("Takip isteği tarihi boş geçilemez ve bugüne eşit veya ileri olmalıdır");
+            RuleFor(x => x.IsActive).NotNull().NotEmpty().WithMessage("Aktiflik durumu boş geçilemez");
+        }
+    }
+}
