525d6f53eee4ea4d8939303cdc7d98098ab8c930
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2020-01-19 00:05:25 +0300

    Fluentvalidation fix

--- FFinder.BLL/Validators/Comment/UpdateCommentValidator.cs
+++ FFinder.BLL/Validators/Comment/UpdateCommentValidator.cs
@@ -10,14 +10,14 @@ namespace FFinder.BLL.Validators.Comment
     {
         public UpdateCommentValidator()
         {
-            RuleFor(x => x.CommentId).NotEmpty().WithMessage("CommentId belirtilmelidir.");
-            RuleFor(x => x.CommentBody).NotEmpty().WithMessage("Yorum içeriği belirtilmelidir.");
+            RuleFor(x => x.CommentId).NotNull().NotEmpty().WithMessage("CommentId belirtilmelidir.");
+            RuleFor(x => x.CommentBody).NotNull().NotEmpty().WithMessage("Yorum içeriği belirtilmelidir.");
             RuleFor(x => x.CommentBody).MaximumLength(250)
                     .WithMessage("Yorum içeriği maksimum 250 karakterden oluşmalıdır");
-            RuleFor(x => x.PostId).NotEmpty().WithMessage("PostId belirtilmelidir.");
-            RuleFor(x => x.CommentDate).NotEmpty().WithMessage("CommentDate belirtilmelidir.");
-            RuleFor(x => x.IsActive).NotEmpty().WithMessage("Yorumun aktiflik durumu belirtilmelidir.");
-            RuleFor(x => x.OwnerId).NotEmpty().WithMessage("OwnerId belirtilmelidir.");
+            RuleFor(x => x.PostId).NotNull().NotEmpty().WithMessage("PostId belirtilmelidir.");
+            RuleFor(x => x.CommentDate).NotNull().NotEmpty().WithMessage("CommentDate belirtilmelidir.");
+            RuleFor(x => x.IsActive).NotNull().WithMessage("Yorumun aktiflik durumu belirtilmelidir.");
+            RuleFor(x => x.OwnerId).NotNull().NotEmpty().WithMessage("OwnerId belirtilmelidir.");
 
         }
 
--- FFinder.BLL/Validators/CommentRate/UpdateCommentRateValidator.cs
+++ FFinder.BLL/Validators/CommentRate/UpdateCommentRateValidator.cs
@@ -12,9 +12,9 @@ namespace FFinder.BLL.Validators.CommentRate
         {
             RuleFor(x => x.CommentRateId).NotEmpty().WithMessage("CommentId belirtilmelidir.");
             RuleFor(x => x.CommentId).NotEmpty().WithMessage("CommentId belirtilmelidir.");
-            RuleFor(x => x.IsLike).NotEmpty().WithMessage("Yoruma like atılıp atılmadığı belirtilmelidir.");
+            RuleFor(x => x.IsLike).NotNull().WithMessage("Yoruma like atılıp atılmadığı belirtilmelidir.");
             RuleFor(x => x.RateDate).NotEmpty().WithMessage("Rate'e ait date belirtilmelidir.");
-            RuleFor(x => x.IsActive).NotEmpty().WithMessage("Yorumun aktiflik durumu belirtilmelidir.");
+            RuleFor(x => x.IsActive).NotNull().WithMessage("Yorumun aktiflik durumu belirtilmelidir.");
             RuleFor(x => x.OwnerId).NotEmpty().WithMessage("OwnerId belirtilmelidir.");
         }
     }
